01249937648b81309ab65ea6055406d5e6e83c77
beven <bevennyamande@gmail.com>
2015-03-19 12:10:31 +0200

    Update oasis.py

--- oasis.py
+++ oasis.py
@@ -1,38 +1,35 @@
 import sys
 
 class Anti_soft_Drink():
-    print ""
-    print "This is a console program that predicts when a user will be diabetic."
-    ## Introduction By Raindolf ##
-   
-    print ""
-    print ""
-    ## Creating a space for the user to input his or her details ##
-    print "Please enter your full name:"
-    name = raw_input()
-    print "Welcome " + name + ", this is a basic Python program that predicts when u are liable to be diabetic."
-    print ""
     
-    print "Please enter the number of times you take soft drinks in a week: "
-    soft_num = int(input())
+    
+    def blank():
+        print('')
+        print('')
         
-    if 21 <= soft_num <= 30:
-            print "Stop taking soft drinks or you will have yourself to blame once you are diagnosed with diabetes"
-            
-    elif 11 <= soft_num <= 20:
-            print "You have to reduce it to less than 5 in a week or you will be diabetic in not less than 5 years"
-            
-    elif 3 <= soft_num <= 10:
-            print "Please Reduce your intake of soft drinks or in 15 yrs you will be diabetic"
-    elif 1<= soft_num < 3:
-            print "You are safe from diabetes"
         
-    print ""
-    print ""
-    print "Thanks " + name + " , for taking my Anti-Soft drink advice."
-
-
-raw_input("Press <enter> to exit ")
+    blank()
+    print "This is a console program that predicts when a user will be diabetic."
+    blank()
+    query = "Please enter your full name:"
+    name = raw_input(query)
+    print("Welcome  {}, this is a basic Python program that predicts when u are liable to be diabetic.".format(name))
+    blank()
+    query = "Please enter the number of times you take soft drinks in a week: "
+    number = int(input(query))
+        
+        
+    if 21 <= number <= 30:
+            print("Stop taking soft drinks or you will have yourself to blame once you are diagnosed with diabetes")
+    elif 11 <= number <= 20:
+            print("You have to reduce it to less than 5 in a week or you will be diabetic in not less than 5 years")
+    elif 3 <= number <= 10:
+            print("Please Reduce your intake of soft drinks or in 15 yrs you will be diabetic")
+    elif 1<= number < 3:
+            print ("You are safe from diabetes")
+            
+    blank()
+    print("Thanks {} , for taking my Anti-Soft drink advice.".format(name))
 
 ## Send me your emails for more raindolf@oasiswebsoft.com ##
 ## Kindly add us on fb www.fb.com/oasisweb ##
